b7a499226585808ce823db0bd1e3333fec9e1801
michalpostek <michal.postek@protonmail.com>
2025-04-12 19:36:10 +0200

    parameter-encoder: remove unused constructor

--- GeneticAlgorithm/ParameterEncoder.cs
+++ GeneticAlgorithm/ParameterEncoder.cs
@@ -2,27 +2,25 @@
 
 public class ParameterEncoder(int min, int max, int length)
 {
-    private readonly Dictionary<string, double> _valueLookup = CreateValueLookup(min, max, length);    
-    
-    public double GetValue(string chromosomes)
-    {
-        return _valueLookup[chromosomes];
-    }
-    
+    private readonly Dictionary<string, double> _valueLookup = CreateValueLookup(min, max, length);
+
     public double GetValue(bool[] chromosomes)
     {
         return _valueLookup[StringifyChromosomes(chromosomes)];
     }
-    
+
     private static string StringifyChromosomes(bool[] chromosomes)
     {
         var chars = new char[chromosomes.Length];
 
-        for (var i = 0; i < chromosomes.Length; i++) chars[i] = chromosomes[i] ? '1' : '0';
+        for (var i = 0; i < chromosomes.Length; i++)
+        {
+            chars[i] = chromosomes[i] ? '1' : '0';
+        }
 
         return new string(chars);
     }
-    
+
     private static Dictionary<string, double> CreateValueLookup(int min, int max, int chromosomes)
     {
         var lookup = new Dictionary<string, double>();
@@ -32,7 +30,7 @@ public class ParameterEncoder(int min, int max, int length)
         for (var i = 0; i < entries; i++)
         {
             var key = Convert.ToString(i, 2).PadLeft(chromosomes, '0');
-            lookup.Add(key, min + (diff * i));
+            lookup.Add(key, min + diff * i);
         }
 
         return lookup;
